07abe843a923eb09fe64aa94f8312fe4e31e049f
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-09-11 23:45:16 +0100

    Finished sidebar for movie details

--- app/(home)/movies/layout.tsx
+++ app/(home)/movies/layout.tsx
@@ -1,18 +1,12 @@
 import MovieDetailsSideBar from "@/components/Layout/Movies/Sidebar";
-import Footer from "@/components/UI/Footer";
 import "./styles/layout.css";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <div className="detLayout grid">
-        <aside className="min-h-screen fixed md:w-[320px] w-[260px] bg-gray-50">
-          <MovieDetailsSideBar />
-        </aside>
-
-        <main className="p-5 h-screen md:ml-[320px] ml-[260px]">{children}</main>
-
-        {/* <Footer /> */}
+      <div className="detLayout grid gap-4">
+        <MovieDetailsSideBar />
+        <main className="p-5 h-screen md:ml-[280px] sm:ml-[200px]">{children}</main>
       </div>
     </>
   );
--- components/Layout/Movies/Sidebar/index.tsx
+++ components/Layout/Movies/Sidebar/index.tsx
@@ -1,7 +1,45 @@
+import Image from "next/image";
+
 const MovieDetailsSideBar = () => {
+  const sideLinks = [
+    { label: "Home", icon: <i className="ri-home-2-line"></i> },
+    { label: "Movies", icon: <i className="ri-movie-2-line"></i> },
+    { label: "TV Series", icon: <i className="ri-tv-2-line"></i> },
+    { label: "Upcoming", icon: <i className="ri-calendar-2-line"></i> },
+  ];
+
   return (
     <>
-      <p>Sidebar</p>
+      <aside className="min-h-screen fixed overflow-y-auto md:w-[280px] sm:w-[200px] rounded-tr-3xl rounded-br-rounded-tr-3xl border-2 space-y-14">
+        <div className="flex space-x-4 items-center p-4">
+          <Image src={`/images/others/movie_box_logo.png`} width={50} height={50} alt="Logo" />
+          <span className="font-bold text-xl">MovieMex</span>
+        </div>
+
+        <div className="space-y-2">
+          {sideLinks.map((link, idx) => (
+            <div
+              key={idx}
+              className={`flex space-x-2 p-5 duration-200 cursor-pointer ${
+                link.label === "Movies" ? "border-r-[6px] border-rose-600 bg-rose-50 hover:bg-rose-100" : "hover:bg-rose-50"
+              }`}
+            >
+              {link.icon} <p>{link.label}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="border border-rose-600 rounded-3xl bg-rose-50 pt-10 px-4 pb-4 space-y-2 mx-2 p-4">
+          <h4 className="font-semibold text-lg leading-relaxed">Play more quizzes to earn free tickets</h4>
+          <p className="text-gray-700">50k People are playing now.</p>
+
+          <div className="text-center">
+            <button className="text-rose-600 bg-rose-300 rounded-full px-3 py-1 text-sm font-semibold">
+              Start Playing
+            </button>
+          </div>
+        </div>
+      </aside>
     </>
   );
 };
